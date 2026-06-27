269cc73e53207d6ed5dbeb5d19da78992797e80e
nehakumbhar-collab <neha.kumbhar@cumminscollege.in>
2026-06-27 11:17:51 +0530

    update api endpoints

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT = "http://localhost:5000/api/auth";
-export const JOB_API_END_POINT = "http://localhost:5000/api/job";
-export const COMPANY_API_END_POINT = "http://localhost:5000/api/company";
-export const APPLICATION_API_END_POINT = "http://localhost:5000/api/application";
+export const USER_API_END_POINT = "https://job-portal-backend-c6v8.onrender.com/api/auth";
+export const JOB_API_END_POINT = "https://job-portal-backend-c6v8.onrender.com/api/job";
+export const COMPANY_API_END_POINT = "https://job-portal-backend-c6v8.onrender.com/api/company";
+export const APPLICATION_API_END_POINT = "https://job-portal-backend-c6v8.onrender.com/api/application";
